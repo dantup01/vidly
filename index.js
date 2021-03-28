c0440a055cb65d9bd9e7b0547e1c6ad972c66aa4
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-03-28 18:12:01 +0100

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ const Joi = require("joi");
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
+const genres = [
+    { id: 1, name: 'Action' },
+    { id: 2, name: 'Comedy' },
+    { id: 3, name: 'Superhero' },
+    { id: 4, name: 'Drama' },
+    { id: 5, name: 'Scifi' },
+];
+
+app.get('api/genres', (req, res) => {
+    res.send(genres);
+});
+
+app.post('api/genres/', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    res.send(genre);
+});
 
 function validateGenre(genre) {
     const schema = {
